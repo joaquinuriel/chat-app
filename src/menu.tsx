eb789b468cbd6481b02322a736f7066ba172d4e5
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2021-08-18 14:59:06 -0300

    update

--- src/menu.tsx
+++ src/menu.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 import { createRef, useState } from "react";
 
 export default function Menu(props) {
-  const [visible, setVisible] = useState(props.visible);
-
+  const classList = `menu ${props.visible ? "visible" : ""}`;
   const ref = createRef<HTMLElement>();
-  // const handleClick = (e) => {
-  //   if (ref.current && !ref.current.contains(e.target)) setVisible(false);
-  // };
-  // addEventListener("click", handleClick);
+  const menuCondition = (t) => ref.current && !ref.current.contains(t);
+  const btnCondition = (t) =>
+    props.btn.current && !props.btn.current.contains(t);
+  const handleClick = ({ target }) => {
+    menuCondition(target) && btnCondition(target) && props.onClickOut();
+  };
+  addEventListener("click", handleClick);
   return (
-    <aside ref={ref} className={`menu ${visible && "visible"}`}>
+    <aside ref={ref} className={classList}>
       {/* <Image src={props.photo} /> */}
       <h2>{props.name}</h2>
       {/* <p>{props.email}</p> */}

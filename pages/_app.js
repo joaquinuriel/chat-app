7b2fb14d7cb662dc5a2cd00bb058f1386bf39c1e
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2021-08-25 19:02:44 -0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,13 @@ import { ProvideAuth } from "src/auth";
 import "styles/globals.sass";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => void navigator.serviceWorker.register("/sw.js"), []);
+  useEffect(() => {
+    navigator.serviceWorker.register("/sw.js");
+    [
+      document.querySelectorAll("button"),
+      document.querySelectorAll("a"),
+    ].forEach((el) => (el.onTouchStart = null));
+  }, []);
   return (
     <ProvideAuth>
       <Component {...pageProps} />
